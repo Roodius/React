2a3641616dcd6ce9d9ae648be9e84efe6d356e44
OsmanXwiinder <osmansaifi30@gmail.com>
2025-04-15 13:14:19 +0530

    todo-app

--- first-Project/src/App.jsx
+++ first-Project/src/App.jsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
+
 
 function App(){
   const [count, setCount] = useState(0)
-  //onpress function
-  function onClickhandler(){
-    
-    console.log()
-  }
-
   return (
     <div>
-      <button onClick={onClickhandler}>Counter {}</button>
+      <CustomButton count={count} setCount = {setCount}></CustomButton>
     </div>
   )
 }
 
+
+ // button component  which takes some state as input  and render into button content
+function CustomButton(props){
+  //onpress function
+  function onClickhandler(){
+    props.setCount(props.count+1);
+  } 
+
+  return React.createElement(
+    'button',
+    {onClick:onClickhandler},
+    `Counter ${props.count}`
+  )
+}
 
 
 export default App
---
+++ todo-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+function App(){
+  const [todo, setTodo] = useState([]);
+  return (
+    <div>
+       
+    </div>
+  )
+}
+
+export default App
