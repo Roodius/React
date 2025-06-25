d854b0df2b62a6c6b77e2a29f8bd4e68f2cee1b7
OsmanXwiinder <osmansaifi30@gmail.com>
2025-06-25 16:15:01 +0530

    mind now remembering things

--- react-part-2/src/App.jsx
+++ react-part-2/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import {useState}  from "react";
+
+
+function App(){
+  const [currTitle, SetcurrTitle] = useState("osman1")
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <HeaderWithButton/>
+      <Header title = {currTitle}></Header>
+      <Header title = "osman2"></Header>
+      
     </>
   )
 }
 
-export default App
+
+function HeaderWithButton(){
+  const [firstTitle, setfirstTitle] = useState("my name is osman saifi");
+  
+  function changeTitle(){
+    setfirstTitle("my name is  " + Math.random())
+}        
+  return <>
+  <button onClick={changeTitle}>click me To the Change the Content</button>
+  <Header title = {firstTitle}/>
+  </>
+}
+
+
+function Header({title}){
+  console.log("Rendered");
+  return <div> 
+        <h1>{title}</h1>
+      </div>
+}
+
+export default App;
