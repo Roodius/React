daf49723b5a65499f7cff3608888971f955f14d6
OsmanXwiinder <osmansaifi30@gmail.com>
2025-06-27 15:50:25 +0530

    wrapper Component Done

--- react-part-2/src/App.jsx
+++ react-part-2/src/App.jsx
@@ -1,42 +1,51 @@
-// import { useState } from "react";
-
-import {useState}  from "react";
 
 
 function App(){
-  const [currTitle, SetcurrTitle] = useState("osman1")
 
+  return <div style={{display:"flex", alignItems:"center", justifyContent:"center"}}>
+    <CardWrapper>
+      <div>
+      <Hello/>
+      </div>
+    </CardWrapper>
 
+  <CardWrapper>
+    <By />
+  </CardWrapper>
 
-  return (
-    <>
-      <HeaderWithButton/>
-      <Header title = {currTitle}></Header>
-      <Header title = "osman2"></Header>
-      
-    </>
-  )
+  </div>
 }
 
-
-function HeaderWithButton(){
-  const [firstTitle, setfirstTitle] = useState("my name is osman saifi");
-  
-  function changeTitle(){
-    setfirstTitle("my name is  " + Math.random())
-}        
+function By(){
   return <>
-  <button onClick={changeTitle}>click me To the Change the Content</button>
-  <Header title = {firstTitle}/>
+    By There
   </>
 }
 
-
-function Header({title}){
-  console.log("Rendered");
-  return <div> 
-        <h1>{title}</h1>
-      </div>
+function Hello(){
+    return <>
+      hi there
+      </>
 }
 
-export default App;
+function CardWrapper({children}){
+      return <div style={{border:"black 2px solid", height:"300px", width:"250px", alignItems:"center", display:"flex", justifyContent:"center", margin:"10px",padding:"10px"}}>
+         {children}
+      </div>
+}
+// function TextComponent(){x
+//     return <div>
+//           <p>This is a Text Component</p> 
+//     </div>
+// }
+
+// function TextComponent2(){
+//   return <div>
+//     <p>This is a Component 2</p>
+//   </div>
+// }
+
+
+
+
+export default App; 
