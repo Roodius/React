2b5f5d78d2917405e0747bffbba064252f73242a
OsmanXwiinder <osmansaifi30@gmail.com>
2025-07-04 17:08:24 +0530

    react part 3

--- react-part-2/src/App.jsx
+++ react-part-2/src/App.jsx
@@ -1,44 +1,27 @@
-import { useEffect, useState } from "react"
+import { memo, useCallback, useEffect, useState } from "react"
+import axios from "axios"
 
 
-function App(){
-  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im9zbWFuIiwiaWF0IjoxNzUxMDM0NzAzfQ.RNhoK7KGbB_ILHzdeguCEeZ7__UuB70iUxtxgnkDbuY";
-  const [todos, setTodos] = useState([]);
-  
+function useTodos(){
+  const [todos, settodos] = useState([]);
+
   useEffect(() => {
-    console.log("use Effect Runs")
-     setTimeout(() => {
-        fetch("http://localhost:3000/todo/Gettodo", {
-      method:"GET",
-      headers:{
-        'Authorization': `Bearer ${token}`,
-      }
-      }).then((res) => {
-      if(!res.ok) throw new Error(`HTTP${res.status}`);
-      return res.json();
-    }).then((data) => {
-      setTodos(data.todos)
-      console.log(data)
-    }).catch(err => {
-      console.log("Fetch Error", err)
+    axios.get("http://localhost:3000/todo/Gettodo/")
+    .then((res) => {
+        settodos(res.data.todos)
     })
-  }, 3000)
-    
   },[])
 
-  return <> 
-    {Array.isArray(todos) && todos.map((todo,i) => (
-      <Todo key={i} title={todo.title} description={todo.description}/>
-    ))}
+  return todos; 
+}
+
+function App(){
+    const todos = useTodos();
+  
+
+  return <>
+    {todos}
   </>
 }
-
-function Todo({title,description}){
-  return <div>
-    <h1>{title}</h1>
-    <p>{description}</p>
-  </div>
-}
-
 
 export default App;
