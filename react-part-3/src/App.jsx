0e49c4011842fb10fd69c997f3c5d6f9417755ae
OsmanXwiinder <osmansaifi30@gmail.com>
2025-07-08 15:00:36 +0530

    prop drilling

--- react-part-3/src/App.jsx
+++ react-part-3/src/App.jsx
@@ -1,39 +1,32 @@
-import { BrowserRouter,Route, Routes, useNavigate } from "react-router-dom"
-// import {Landing}  from "./components/Landing";
-// import { Dashboard }from "./components/Dashboard"
-import {lazy, Suspense} from "react";
-const Dashboard = lazy(() => import("./components/Dashboard"))
-const Landing = lazy(() => import("./components/Landing"))
- // React.lazy(() => import("./dfref/fef"))     
+import { useState } from "react";
+
+
 function App(){
-      
+   const [count, setCount] = useState(0);   
   return <div>
-        <BrowserRouter>
-        <Appbar/>
-          <Routes>
-            <Route path="/dashboard" element={
-              <Suspense fallback={"loading..."}><Dashboard /></Suspense>
-            }/>
-            <Route path="/" element={<Suspense fallback={"loading..."}><Landing/></Suspense>}/>
-          </Routes>
-        </BrowserRouter>
+    <Count count={count}/>
+    <Buttons count={count} setCount={setCount}/>
   </div> 
 
 }
 
-
-function Appbar(){
-const navigate = useNavigate();
+function Count({count}){
 
   return <div>
-    <div>
-      <button onClick={() => {
-        navigate("/dashboard")
-      }}>Dashboard</button>
-      <button onClick={() => {
-        navigate("/")
-      }}>Landing</button>
-    </div>  
+     Your Count : {count}
+  </div>
+
+}
+
+function Buttons({count, setCount}){
+
+  return <div>
+    <button onClick={() => {setCount(count+1)}}>Increase</button>
+    <button onClick={() => {
+      if(count == 0){
+        return
+      }
+      setCount(count-1)}}>Decrease</button>
   </div>
 }
 
