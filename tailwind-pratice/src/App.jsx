16c095c6329a2654606a6f4891f0ad19dab6f736
OsmanXwiinder <osmansaifi30@gmail.com>
2025-07-18 17:38:45 +0530

    tailwind

---
+++ tailwind-pratice/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+
+function App(){
+
+
+  return <div>
+
+    <div className="flex justify-center">
+        <div className="bg-red-500">red</div>
+        <div className="bg-yellow-500">yellow</div>
+        <div className="bg-green-600">green</div>
+    </div>
+
+    
+
+  </div>
+}
+
+export default App;
